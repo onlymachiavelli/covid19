2545a16c011eaf4dd920958d99c817a9388cdeed
Makaveli <tmakaveli643@gmail.com>
2020-03-24 00:24:39 +0100

    Makavelis

---
+++ ar/function.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#rss").click(function(){
+    $(".res-nav").slideToggle()
+  })
+})
+
+
+function wiki(){
+        window.location='https://en.wikipedia.org/wiki/Coronavirus_disease_2019';
+      }
+
+
+function mail(){
+  alert("tmakaveli643@gmail.com");
+}
+function en(){
+    window.location='../index.html';
+}
---
+++ myhealth/function.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#rss").click(function(){
+    $(".res-nav").slideToggle()
+  })
+})
